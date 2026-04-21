8b48b1923537dc415eba28cb02dcc0abf5df02f0
Dave Coleman <dave@dvclmn.com>
2026-04-21 11:07:02 +0200

    Moves some Env values to CanvasKit. Will need to add CanvasCore as dep

--- Sources/BasePrimitives/Environment/ComponentEnvironmentValues.swift
+++ Sources/BasePrimitives/Environment/ComponentEnvironmentValues.swift
@@ -82,20 +82,6 @@ extension EnvironmentValues {
   @Entry public var panOffset: CGSize = .zero
   @Entry public var rotation: Angle = .zero
 
-  /// Important: This zoom level is not clamped. Use ``zoomClamped``
-  /// which clamps by ``zoomRange`` if clamping is required
-
-  @Entry public var zoomLevel: Double = 1.0
-  
-  /// This was previously optional, but trying out a default value instead
-  @Entry public var zoomRange: ClosedRange<Double> = 0.2...10
-
-  /// Will return unclamped if no zoom range found in the Environment
-  public var zoomClamped: Double {
-    guard zoomLevel.isFiniteAndGreaterThanZero else { return 1.0 }
-    return zoomLevel.clamped(to: zoomRange)
-  }
-
   @Entry public var pointerStyle: PointerStyleCompatible?
 
 }
@@ -110,4 +96,3 @@ extension EnvironmentValues {
   @Entry public var isAnimationEnabled: Bool = false
 
 }
-
--- Sources/BasePrimitives/Extensions/BinaryFloatingPoint/BFP+Checks.swift
+++ Sources/BasePrimitives/Extensions/BinaryFloatingPoint/BFP+Checks.swift
@@ -8,19 +8,19 @@
 import Foundation
 
 extension BinaryFloatingPoint {
-  
+
   /// Checks that this value is neither infinite, nor Nan
   public var isValid: Bool {
     return self.isFinite && !self.isNaN
   }
-  
+
   /// Returns `true` if this value falls within the specified closed range (inclusive of bounds).
   /// - Parameter range: The closed range to check against
   /// - Returns: `true` if the value is within the range, `false` otherwise
   public func isWithin(_ range: ClosedRange<Self>) -> Bool {
     return range.contains(self)
   }
-  
+
   /// Returns `true` if this value falls within the specified bounds (inclusive).
   /// - Parameters:
   ///   - lowerBound: The lower bound (inclusive)
@@ -29,13 +29,12 @@ extension BinaryFloatingPoint {
   public func isWithin(_ lowerBound: Self, _ upperBound: Self) -> Bool {
     return self >= lowerBound && self <= upperBound
   }
-  
+
   public var isGreaterThanZero: Bool { self > 0 }
   public var isFiniteAndGreaterThanZero: Bool { isFinite && self > 0 }
   public var isGreaterThanOrEqualToZero: Bool { self >= 0 }
-  
-}
 
+}
 
 //import Foundation
 //
