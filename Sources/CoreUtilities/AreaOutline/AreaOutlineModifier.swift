007e55a90900ac2accca75d696e7badd0a2340a5
Dave Coleman <dave@dvclmn.com>
2026-04-27 10:00:56 +0200

    Removes zoom/pan etc from Env values, these have gone to CanvasKit

--- Sources/CoreUtilities/AreaOutline/AreaOutlineModifier.swift
+++ Sources/CoreUtilities/AreaOutline/AreaOutlineModifier.swift
@@ -5,62 +5,62 @@
 //  Created by Dave Coleman on 5/4/2026.
 //
 
-import SwiftUI
+//import SwiftUI
 
-struct AreaOutlineModifier: ViewModifier {
-//  @Environment(\.self) private var env
-  @Environment(\.zoomLevel) private var zoomLevel
-  @Environment(\.zoomRange) private var zoomRange
-
-  let colour: Color
-  let rounding: Double
-  let lineWidth: Double
-  let sensitivity: Double?
-//  let outline: AreaOutline
-
-  func body(content: Content) -> some View {
-    content
-      .overlay {
-        RoundedRectangle(
-          cornerRadius: rounding.removingZoom(
-            zoomLevel,
-            across: zoomRange,
-            sensitivity: sensitivity,
-          )
-        )
-        .fill(.clear)
-        .stroke(
-          colour,
-          lineWidth: lineWidth.removingZoom(
-            zoomLevel,
-            across: zoomRange,
-            sensitivity: sensitivity,
-          ),
-        )
-        .allowsHitTesting(false)
-      }
-//      .overlay { AreaOutlineShape(outline) }
-  }
-}
-
-extension View {
-  public func areaOutline(
-    colour: Color = .white.opacity(0.07),
-    rounding: Double = 4,
-    lineWidth: Double = 1,
-  ) -> some View {
-    self.modifier(
-      AreaOutlineModifier(
-        colour: colour,
-        rounding: rounding,
-        lineWidth: lineWidth,
-        sensitivity: nil
-//        outline: .init(
-//          colour: colour,
-//          rounding: rounding,
-//          lineWidth: lineWidth,
+//struct AreaOutlineModifier: ViewModifier {
+////  @Environment(\.self) private var env
+////  @Environment(\.zoomLevel) private var zoomLevel
+////  @Environment(\.zoomRange) private var zoomRange
+//
+//  let colour: Color
+//  let rounding: Double
+//  let lineWidth: Double
+//  let sensitivity: Double?
+////  let outline: AreaOutline
+//
+//  func body(content: Content) -> some View {
+//    content
+//      .overlay {
+//        RoundedRectangle(
+//          cornerRadius: rounding.removingZoom(
+//            zoomLevel,
+//            across: zoomRange,
+//            sensitivity: sensitivity,
+//          )
 //        )
-      )
-    )
-  }
-}
+//        .fill(.clear)
+//        .stroke(
+//          colour,
+//          lineWidth: lineWidth.removingZoom(
+//            zoomLevel,
+//            across: zoomRange,
+//            sensitivity: sensitivity,
+//          ),
+//        )
+//        .allowsHitTesting(false)
+//      }
+////      .overlay { AreaOutlineShape(outline) }
+//  }
+//}
+
+//extension View {
+//  public func areaOutline(
+//    colour: Color = .white.opacity(0.07),
+//    rounding: Double = 4,
+//    lineWidth: Double = 1,
+//  ) -> some View {
+//    self.modifier(
+//      AreaOutlineModifier(
+//        colour: colour,
+//        rounding: rounding,
+//        lineWidth: lineWidth,
+//        sensitivity: nil
+////        outline: .init(
+////          colour: colour,
+////          rounding: rounding,
+////          lineWidth: lineWidth,
+////        )
+//      )
+//    )
+//  }
+//}
--- Sources/CoreUtilities/Environment/ZoomPanEnv.swift
+++ Sources/CoreUtilities/Environment/ZoomPanEnv.swift
@@ -5,25 +5,11 @@
 //  Created by Dave Coleman on 21/4/2026.
 //
 
-import SwiftUI
-
-extension EnvironmentValues {
-
-  @Entry public var panOffset: CGSize = .zero
-  @Entry public var rotation: Angle = .zero
-
-  /// Important: This zoom level is not clamped. Use ``zoomClamped``
-  /// (which clamps by ``zoomRange``) if clamping is required
-  @Entry public var zoomLevel: Double = 1.0
-
-  @Entry public var zoomRange: ClosedRange<Double> = 0.2...10
-
-  /// Returns `1.0` if `zoomLevel` is less than zero or NaN/infinite
-  public var zoomClamped: Double {
-    guard zoomLevel.isFiniteAndGreaterThanZero else { return 1.0 }
-    return zoomLevel.clamped(to: zoomRange)
-  }
-}
+//import SwiftUI
+//
+//extension EnvironmentValues {
+//
+//}
 
 
 //static let minZoomLowerBound: Double = 0.05
