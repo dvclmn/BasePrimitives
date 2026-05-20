fb346875b99678c7052255d3dac9e19c8ab57a61
Dave Coleman <dave@dvclmn.com>
2026-05-20 11:58:43 +0200

    Removes the Color opacity level presets

--- Sources/BasePrimitives/Extensions/Color/Color.swift
+++ Sources/BasePrimitives/Extensions/Color/Color.swift
@@ -11,16 +11,6 @@ extension Color {
   public var toShapeStyle: AnyShapeStyle { AnyShapeStyle(self) }
 }
 
-enum OpacityPreset: CGFloat {
-  case opacityBarelyThere = 0.03
-  case opacityFaint = 0.1
-  case opacityLow = 0.3
-  case opacityMid = 0.5
-  case opacityMedium = 0.65
-  case opacityHigh = 0.85
-  case opacityNearOpaque = 0.9
-}
-
 extension Color {
   
   public static let darkGrey: Color = .init(white: 0.28)
@@ -29,14 +19,6 @@ extension Color {
   public func toCGColor(using environment: EnvironmentValues) -> CGColor{
     resolve(in: environment).cgColor
   }
-
-  public var opacityBarelyThere: Color { opacity(OpacityPreset.opacityBarelyThere.rawValue) }
-  public var opacityFaint: Color { opacity(OpacityPreset.opacityFaint.rawValue) }
-  public var opacityLow: Color { opacity(OpacityPreset.opacityLow.rawValue) }
-  public var opacityMid: Color { opacity(OpacityPreset.opacityMid.rawValue) }
-  public var opacityMedium: Color { opacity(OpacityPreset.opacityMedium.rawValue) }
-  public var opacityHigh: Color { opacity(OpacityPreset.opacityHigh.rawValue) }
-  public var opacityNearOpaque: Color { opacity(OpacityPreset.opacityNearOpaque.rawValue) }
 
   public static var random: Color { self.random() }
 
@@ -50,10 +32,3 @@ extension Color {
   }
   public var toNSColor: NSColor { NSColor(self) }
 }
-
-
-//extension Optional where Wrapped == Color {
-//  public func withFallback(_ colour: Color) -> AnyShapeStyle {
-//    self ?? AnyShapeStyle(colour)
-//  }
-//}
