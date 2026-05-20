ef3e291e89a64fbccbcd11918899629750e65466
Dave Coleman <dave@dvclmn.com>
2026-05-20 10:35:03 +0200

    Removes unused Step size related code. Moves FileSystem utils to Core

--- Sources/BasePrimitives/Utilities/Zeroable.swift
+++ Sources/BasePrimitives/Utilities/Zeroable.swift
@@ -7,21 +7,23 @@
 
 import Foundation
 
-extension Optional where Wrapped: BinaryFloatingPoint {
+// TODO: Hoping to remove this entirely, presumed unused
 
-  /// Applies an operation to two optional floats, treating nil as zero
-  public func combined(
-    with other: Wrapped?,
-    using operation: (Wrapped, Wrapped) -> Wrapped
-  ) -> Wrapped? {
-    guard self != nil || other != nil else { return nil }
-
-    let lhs = self ?? .zero
-    let rhs = other ?? .zero
-    return operation(lhs, rhs)
-  }
-
-  private func withDefault(_ defaultValue: Wrapped) -> Wrapped {
-    return self ?? defaultValue
-  }
-}
+//extension Optional where Wrapped: BinaryFloatingPoint {
+//
+//  /// Applies an operation to two optional floats, treating nil as zero
+//  public func combined(
+//    with other: Wrapped?,
+//    using operation: (Wrapped, Wrapped) -> Wrapped
+//  ) -> Wrapped? {
+//    guard self != nil || other != nil else { return nil }
+//
+//    let lhs = self ?? .zero
+//    let rhs = other ?? .zero
+//    return operation(lhs, rhs)
+//  }
+//
+//  private func withDefault(_ defaultValue: Wrapped) -> Wrapped {
+//    return self ?? defaultValue
+//  }
+//}
