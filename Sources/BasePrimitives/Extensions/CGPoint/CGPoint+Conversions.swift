ae3c38eedca1a15c8edddad3428306adf1f37935
Dave Coleman <dave@dvclmn.com>
2026-05-20 10:31:37 +0200

    Removes unused AreaOutline files

--- Sources/BasePrimitives/Extensions/CGPoint/CGPoint+Conversions.swift
+++ Sources/BasePrimitives/Extensions/CGPoint/CGPoint+Conversions.swift
@@ -45,4 +45,32 @@ extension CGPoint {
   public static func angle(from p1: CGPoint, to p2: CGPoint) -> Angle {
     Angle(radians: angleInRadians(from: p1, to: p2))
   }
+  
+  
+  /// Returns the relative UnitPoint (not snapped) within a given size.
+  ///
+  /// Usage:
+  /// ```
+  /// let point = CGPoint(x: 10, y: 5)
+  /// let frameSize = CGSize(width: 100, height: 100)
+  ///
+  /// let closestAnchor = point.nearestAnchor(in: frameSize)
+  /// print(closestAnchor)  // Might print `.topLeading` if point is near (0,0)
+  /// ```
+  public func toUnitPoint(
+    in size: CGSize,
+    rounding: FloatingPointRoundingRule = .down,
+    fallback: UnitPoint = .center
+  ) -> UnitPoint {
+    
+    guard size.isGreaterThanOrEqualToZero else { return fallback }
+    
+    let rawX = x / size.width
+    let rawY = y / size.height
+    
+    let valX = rawX.rounded(rounding)
+    let valY = rawY.rounded(rounding)
+    
+    return UnitPoint(x: valX, y: valY)
+  }
 }
